39ef4518e464dc2599937786bdf3679965b5caed
Douglas Vieira <douglasdotv@hotmail.com>
2023-09-16 13:54:13 -0300

    backend: enable CORS for Product and ProductCategory repositories
    Add @CrossOrigin annotation to allow frontend access from localhost:4200

--- backend/ecommerce/src/main/java/br/com/dv/ecommerce/dao/ProductRepository.java
+++ backend/ecommerce/src/main/java/br/com/dv/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package br.com.dv.ecommerce.dao;
 
 import br.com.dv.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }

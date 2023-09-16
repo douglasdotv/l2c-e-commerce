d1b9c3c16b35e4d1b361719d691ae7b56581f674
Douglas Vieira <douglasdotv@hotmail.com>
2023-09-16 10:26:03 -0300

    backend: restrict mutable HTTP methods for API
    Configured the REST API to limit certain HTTP methods for Product and ProductCategory entities. Disabled PUT, POST, and DELETE actions.

---
+++ backend/ecommerce/src/main/java/br/com/dv/ecommerce/config/DataRestConfig.java
@@ -0,0 +1,35 @@
+package br.com.dv.ecommerce.config;
+
+import br.com.dv.ecommerce.entity.Product;
+import br.com.dv.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] unsupportedActions = {
+                HttpMethod.PUT,
+                HttpMethod.POST,
+                HttpMethod.DELETE
+        };
+
+        // Disable HTTP methods for Product (PUT, POST and DELETE)
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        // Disable HTTP methods for ProductCategory (PUT, POST and DELETE)
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+    }
+
+}

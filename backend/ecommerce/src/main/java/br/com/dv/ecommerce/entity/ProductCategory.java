06e1485ee8998258619407e64d145aadd9458b8b
Douglas Vieira <douglasdotv@hotmail.com>
2023-09-20 13:55:04 -0300

    backend: add missing @Column annotation to ProductCategory id field

--- backend/ecommerce/src/main/java/br/com/dv/ecommerce/entity/ProductCategory.java
+++ backend/ecommerce/src/main/java/br/com/dv/ecommerce/entity/ProductCategory.java
@@ -14,6 +14,7 @@ public class ProductCategory {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Long id;
 
     @Column(name="category_name")

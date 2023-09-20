173b91f599979c74adfbd062718387e807035636
Douglas Vieira <douglasdotv@hotmail.com>
2023-09-20 19:28:48 -0300

    backend: expose entity IDs in DataRestConfig

--- backend/ecommerce/src/main/java/br/com/dv/ecommerce/config/DataRestConfig.java
+++ backend/ecommerce/src/main/java/br/com/dv/ecommerce/config/DataRestConfig.java
@@ -2,6 +2,8 @@ package br.com.dv.ecommerce.config;
 
 import br.com.dv.ecommerce.entity.Product;
 import br.com.dv.ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -10,6 +12,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
+
+    private final EntityManager entityManager;
+
+    public DataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -30,6 +38,17 @@ public class DataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
                 .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        // Expose entity ids
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        Class<?>[] domainTypes = entityManager.getMetamodel().getEntities().stream()
+                .map(EntityType::getJavaType)
+                .toArray(Class<?>[]::new);
+
+        config.exposeIdsFor(domainTypes);
     }
 
 }

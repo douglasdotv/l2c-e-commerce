6c516e2dedff5db810c292ff705e3443bf8bc193
Douglas Vieira <douglasdotv@hotmail.com>
2023-09-21 09:08:59 -0300

    frontend: implement getProductCategories() in ProductService
    Updated 'ProductService' to include a new method 'getProductCategories' for fetching product categories.
    Adjusted the base URL in 'ProductService' to 'http://localhost:8080/api'.

--- frontend/ecommerce/src/app/services/product.service.ts
+++ frontend/ecommerce/src/app/services/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -8,21 +9,35 @@ import { map } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class ProductService {
-  private readonly baseUrl = 'http://localhost:8080/api/products';
+  private readonly baseUrl = 'http://localhost:8080/api';
 
   constructor(private httpClient: HttpClient) {}
 
   getProductList(categoryId: number): Observable<Product[]> {
-    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
+    const searchUrl = `${this.baseUrl}/products/search/findByCategoryId?id=${categoryId}`;
 
     return this.httpClient
-      .get<GetResponse>(searchUrl)
+      .get<GetResponseProducts>(searchUrl)
       .pipe(map((response) => response._embedded.products));
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    const searchUrl = `${this.baseUrl}/product-category`;
+
+    return this.httpClient
+      .get<GetResponseProductCategory>(searchUrl)
+      .pipe(map((response) => response._embedded.productCategory));
   }
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  };
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   };
 }

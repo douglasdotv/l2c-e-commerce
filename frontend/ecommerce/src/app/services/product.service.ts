1b50b354a0d91540097b22bb8847109e3b3df85b
Douglas Vieira <douglasdotv@hotmail.com>
2023-09-21 12:38:28 -0300

    frontend: implement search by keyword method in ProductService

--- frontend/ecommerce/src/app/services/product.service.ts
+++ frontend/ecommerce/src/app/services/product.service.ts
@@ -28,6 +28,14 @@ export class ProductService {
       .get<GetResponseProductCategory>(searchUrl)
       .pipe(map((response) => response._embedded.productCategory));
   }
+
+  searchProductsByKeyword(keyword: string): Observable<Product[]> {
+    const searchUrl = `${this.baseUrl}/products/search/findByNameContaining?name=${keyword}`;
+
+    return this.httpClient
+      .get<GetResponseProducts>(searchUrl)
+      .pipe(map((response) => response._embedded.products));
+  }
 }
 
 interface GetResponseProducts {

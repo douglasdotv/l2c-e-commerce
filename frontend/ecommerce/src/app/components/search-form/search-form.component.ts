7e5c7db5dcbe2e9da1cc7d397ff18d7e8c332373
Douglas Vieira <douglasdotv@hotmail.com>
2023-09-21 12:37:49 -0300

    frontend: implement search by keyword method in search-form component

--- frontend/ecommerce/src/app/components/search-form/search-form.component.ts
+++ frontend/ecommerce/src/app/components/search-form/search-form.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
-  styleUrls: ['./search-form.component.css']
+  styleUrls: ['./search-form.component.css'],
 })
-export class SearchFormComponent {
+export class SearchFormComponent implements OnInit {
+  constructor(private router: Router) {}
 
+  ngOnInit(): void {}
+
+  doSearch(value: string) {
+    this.router.navigateByUrl(`/search/${value}`);
+  }
 }

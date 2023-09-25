666442ba50be2b141e3108844aed76ac92cdeab4
Douglas Vieira <douglasdotv@hotmail.com>
2023-09-25 20:17:59 -0300

    frontend: implement addToCart and getCartTotals methods in CartService
    Added methods to add an item to the cart and update its totals.

--- frontend/ecommerce/src/app/services/cart.service.ts
+++ frontend/ecommerce/src/app/services/cart.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { CartItem } from '../common/cart-item';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  cartItems: CartItem[] = [];
 
-  constructor() { }
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+  addToCart(cartItem: CartItem) {
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItem: CartItem | undefined = undefined;
+
+    if (this.cartItems.length > 0) {
+      existingCartItem = this.cartItems.find((item) => item.id === cartItem.id);
+      alreadyExistsInCart = existingCartItem !== undefined;
+    }
+
+    if (!alreadyExistsInCart) {
+      this.cartItems.push(cartItem);
+    } else {
+      existingCartItem!.quantity++;
+    }
+
+    this.getCartTotals();
+  }
+
+  getCartTotals() {
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+
+    for (let cartItem of this.cartItems) {
+      totalPriceValue += cartItem.quantity * cartItem.unitPrice;
+      totalQuantityValue += cartItem.quantity;
+    }
+
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+  }
 }

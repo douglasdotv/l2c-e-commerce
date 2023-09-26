1512dce89ba7799cca38be6d35597184f7f2e076
Douglas Vieira <douglasdotv@hotmail.com>
2023-09-26 17:09:03 -0300

    frontend: implement cart details logic in CartDetailsComponent
    Injected cartService and implemented showCartDetails method to display cart items and cart totals.

--- frontend/ecommerce/src/app/components/cart-details/cart-details.component.ts
+++ frontend/ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-details',
   templateUrl: './cart-details.component.html',
   styleUrls: ['./cart-details.component.css']
 })
-export class CartDetailsComponent {
+export class CartDetailsComponent implements OnInit {
 
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.showCartDetails();
+  }
+
+  showCartDetails() {
+    this.cartItems = this.cartService.cartItems;
+
+    this.cartService.totalPrice.subscribe(
+      price => this.totalPrice = price
+    );
+
+    this.cartService.totalQuantity.subscribe(
+      quantity => this.totalQuantity = quantity
+    );
+
+    this.cartService.getCartTotals();
+  }
 }

78824d4d9c63ee6387fc765351f426927d55a47b
Douglas Vieira <douglasdotv@hotmail.com>
2023-10-01 18:31:16 -0300

    frontend: implement methods for credit card months and years in CheckoutFormService
    Created getCreditCardMonths and getCreditCardYears in
    checkout-form-service to retrieve valid months for the current year and
    valid years.

--- frontend/ecommerce/src/app/services/checkout-form.service.ts
+++ frontend/ecommerce/src/app/services/checkout-form.service.ts
@@ -1,9 +1,33 @@
+import { Observable, of } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CheckoutFormService {
+  private readonly EXPIRATION_YEAR_RANGE: number = 10;
 
-  constructor() { }
+  getCreditCardMonths(): Observable<number[]> {
+    const months: number[] = [];
+    const currentMonth: number = new Date().getMonth() + 1;
+
+    for (let month = currentMonth; month <= 12; ++month) {
+      months.push(month);
+    }
+
+    return of(months);
+  }
+
+  getCreditCardYears(): Observable<number[]> {
+    const years: number[] = [];
+
+    const currentYear: number = new Date().getFullYear();
+    const endYear: number = currentYear + this.EXPIRATION_YEAR_RANGE;
+
+    for (let year = currentYear; year <= endYear; ++year) {
+      years.push(year);
+    }
+
+    return of(years);
+  }
 }

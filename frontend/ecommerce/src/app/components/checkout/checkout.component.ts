4d6de2d03a33378177ef4e18bfccc0c1f64cb1fe
Douglas Vieira <douglasdotv@hotmail.com>
2023-10-01 19:48:30 -0300

    frontend: retrieve credit card months and years in checkout component
    Get data from CheckoutFormService: valid years and valid months for the
    current year.

--- frontend/ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { CheckoutFormService } from 'src/app/services/checkout-form.service';
 
 @Component({
   selector: 'app-checkout',
@@ -8,6 +9,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class CheckoutComponent implements OnInit {
   checkoutFormGroup!: FormGroup;
+  defaultAddress!: any;
   countries: string[] = [
     'Brazil',
     'Canada',
@@ -21,9 +23,13 @@ export class CheckoutComponent implements OnInit {
     'Singapore',
   ];
   cardTypes: string[] = ['Visa', 'MasterCard', 'American Express'];
-  defaultAddress!: any;
+  cardMonths: number[] = [];
+  cardYears: number[] = [];
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private formService: CheckoutFormService
+  ) {}
 
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
@@ -55,8 +61,26 @@ export class CheckoutComponent implements OnInit {
         expirationYear: [''],
       }),
     });
+
     this.defaultAddress =
       this.checkoutFormGroup.controls['billingAddress'].value;
+
+    this.formService.getCreditCardMonths().subscribe((months) => {
+      this.cardMonths = months;
+      if (this.cardMonths.length > 0) {
+        this.checkoutFormGroup
+          .get('creditCardDetails.expirationMonth')
+          ?.setValue(this.cardMonths[0]);
+      }
+    });
+    this.formService.getCreditCardYears().subscribe((years) => {
+      this.cardYears = years;
+      if (this.cardYears.length > 0) {
+        this.checkoutFormGroup
+          .get('creditCardDetails.expirationYear')
+          ?.setValue(this.cardYears[0]);
+      }
+    });
   }
 
   onSubmit(): void {

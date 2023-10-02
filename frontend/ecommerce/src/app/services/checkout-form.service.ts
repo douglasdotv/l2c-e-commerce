c1f3cecabc3f29897d4762f0e571e11084d4a36a
Douglas Vieira <douglasdotv@hotmail.com>
2023-10-02 01:41:11 -0300

    frontend: extend CheckoutFormService to fetch countries and states
    Added methods getCountries and getStatesByCountryCode to CheckoutFormService. Integrated HttpClient for REST API calls to Spring Data REST backend.

--- frontend/ecommerce/src/app/services/checkout-form.service.ts
+++ frontend/ecommerce/src/app/services/checkout-form.service.ts
@@ -1,11 +1,17 @@
-import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of, map } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { Country } from '../common/country';
+import { State } from '../common/state';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CheckoutFormService {
   private readonly EXPIRATION_YEAR_RANGE: number = 10;
+  private readonly baseUrl = 'http://localhost:8080/api';
+
+  constructor(private httpClient: HttpClient) {}
 
   getCreditCardMonths(startMonth: number): Observable<number[]> {
     const months: number[] = [];
@@ -29,4 +35,28 @@ export class CheckoutFormService {
 
     return of(years);
   }
+
+  getCountries(): Observable<Country[]> {
+    const url = `${this.baseUrl}/countries`;
+    return this.httpClient.get<EmbeddedData<Country>>(url).pipe(
+      map((response) => {
+        return response._embedded['countries'];
+      })
+    );
+  }
+
+  getStatesByCountryCode(countryCode: string): Observable<State[]> {
+    const url = `${this.baseUrl}/states/search/findByCountryCode?code=${countryCode}`;
+    return this.httpClient.get<EmbeddedData<State>>(url).pipe(
+      map((response) => {
+        return response._embedded['states'];
+      })
+    );
+  }
+}
+
+interface EmbeddedData<T> {
+  _embedded: {
+    [key: string]: T[];
+  };
 }

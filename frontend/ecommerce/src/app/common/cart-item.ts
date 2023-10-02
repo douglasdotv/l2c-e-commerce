3022beed4fbe848453b192c5ca8b8217f278e20d
Douglas Vieira <douglasdotv@hotmail.com>
2023-10-02 01:29:35 -0300

    frontend: refactor CartItem model
    Added access modifiers for consistency.

--- frontend/ecommerce/src/app/common/cart-item.ts
+++ frontend/ecommerce/src/app/common/cart-item.ts
@@ -1,17 +1,16 @@
 import { Product } from './product';
 
 export class CartItem {
-  id: number = 0;
-  name: string = '';
-  imageUrl: string = '';
-  unitPrice: number = 0;
-  quantity: number = 0;
+  public id: number = 0;
+  public name: string = '';
+  public imageUrl: string = '';
+  public unitPrice: number = 0;
+  public quantity: number = 1;
 
   constructor(product: Product) {
     this.id = product.id;
     this.name = product.name;
     this.imageUrl = product.imageUrl;
     this.unitPrice = product.unitPrice;
-    this.quantity = 1;
   }
 }

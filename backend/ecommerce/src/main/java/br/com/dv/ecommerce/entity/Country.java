e5972b80f5e43eaabebbb0d610ccecc3a42f91a2
Douglas Vieira <douglasdotv@hotmail.com>
2023-10-01 23:33:48 -0300

    backend: create Country and State JPA entities

---
+++ backend/ecommerce/src/main/java/br/com/dv/ecommerce/entity/Country.java
@@ -0,0 +1,30 @@
+package br.com.dv.ecommerce.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "country")
+@Getter
+@Setter
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "country")
+    private Set<State> states;
+
+}

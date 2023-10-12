16c205c1d7e8314b9b1035bab2e248044273f33f
Douglas Vieira <douglasdotv@hotmail.com>
2023-10-12 17:06:33 -0300

    backend: create Order entity

---
+++ backend/ecommerce/src/main/java/br/com/dv/ecommerce/entity/Order.java
@@ -0,0 +1,42 @@
+package br.com.dv.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order")
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "order_tracking_number")
+    private String orderTrackingNumber;
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+
+    @Column(name = "total_quantity")
+    private Integer totalQuantity;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private String dateCreated;
+
+    @Column(name = "last_updated")
+    @UpdateTimestamp
+    private String lastUpdated;
+
+}

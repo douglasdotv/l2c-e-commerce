a3f517a14a103ff4f5fb1db133ce29b0dbe01bf8
Douglas Vieira <douglasdotv@hotmail.com>
2023-10-14 16:06:22 -0300

    frontend: create and define purchase-related classes
    Created the following classes: customer.ts, order.ts, order-item.ts, address.ts and purchase.ts

---
+++ frontend/ecommerce/src/app/common/address.ts
@@ -0,0 +1,9 @@
+export class Address {
+  constructor(
+    public city: string,
+    public state: string,
+    public country: string,
+    public street: string,
+    public zipCode: string
+  ) {}
+}
